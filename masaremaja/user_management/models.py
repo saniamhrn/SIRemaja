2ac480b04778392f1df28bff3adf88e74436c9cc
saniarizqi <sania.rizqi@ui.ac.id>
2024-09-26 19:19:15 +0700

    Revert "feat: initial commit for user management"
    This reverts commit b2f4ea67d2efafd46c25efd2f6c379e9a698ec04.

--- masaremaja/user_management/models.py
+++ masaremaja/user_management/models.py
@@ -1,15 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser):
-    # Additional fields for user management can be added here if needed
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Project Manager', 'Project Manager'),
-        ('Creative Team', 'Creative Team'),
-        ('Client', 'Client'),
-    ]
-    role = models.CharField(max_length=50, choices=ROLE_CHOICES)
-    
-    def __str__(self):
-        return self.name
+# Create your models here.
--- masaremaja/user_management/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('users/', views.user_list, name='user_list'),
-    path('users/create/', views.user_create, name='user_create'),
-    path('users/<int:user_id>/edit/', views.user_update, name='user_update'),
-    path('users/<int:user_id>/delete/', views.user_delete, name='user_delete'),
-]
