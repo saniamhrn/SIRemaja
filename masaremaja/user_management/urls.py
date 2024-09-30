b3ebb1978f8b21fdbe8f1f1391304a9e19ef13a9
saniarizqi <sania.rizqi@ui.ac.id>
2024-10-01 01:12:47 +0700

    fix(urls): fix URL path

--- masaremaja/user_management/urls.py
+++ masaremaja/user_management/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'user_management'
+
 urlpatterns = [
-    path('users/', views.user_list, name='user_list'),
-    path('users/create/', views.user_create, name='user_create'),
-    path('users/<int:user_id>/edit/', views.user_update, name='user_update'),
-    path('users/<int:user_id>/delete/', views.user_delete, name='user_delete'),
+    path('', views.user_list, name='user_list'),
+    path('create/', views.user_create, name='user_create'),
+    path('<int:user_id>/edit/', views.user_update, name='user_update'),
+    path('<int:user_id>/delete/', views.user_delete, name='user_delete'),
 ]
