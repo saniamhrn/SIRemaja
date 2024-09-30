93fed5fd7c9ba4b7dd361cd7a621eaae9753cb7c
saniarizqi <sania.rizqi@ui.ac.id>
2024-10-01 01:02:10 +0700

    feat(admin): register custom admin

--- masaremaja/user_management/admin.py
+++ masaremaja/user_management/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'role', 'is_active']
+    list_filter = ['role', 'is_staff', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'role')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'role', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
