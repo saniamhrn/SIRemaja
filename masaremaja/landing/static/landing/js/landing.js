8a9ef8a492a78966147b13703ae0443d668a352d
saniarizqi <sania.rizqi@ui.ac.id>
2024-11-30 18:56:54 +0700

    fix(js): removes unused code

--- masaremaja/landing/static/landing/js/landing.js
+++ masaremaja/landing/static/landing/js/landing.js
@@ -1,30 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     // First Row: Moves Right
-//     new Swiper('.row-right', {
-//         loop: true,
-//         speed: 5000, // Adjust speed as needed
-//         autoplay: {
-//             delay: 0,
-//             disableOnInteraction: false,
-//         },
-//         slidesPerView: 'auto',
-//         freeMode: true,
-//     });
-
-//     // Second Row: Moves Left
-//     new Swiper('.row-left', {
-//         loop: true,
-//         speed: 5000, // Adjust speed as needed
-//         autoplay: {
-//             delay: 0,
-//             disableOnInteraction: false,
-//             reverseDirection: true,
-//         },
-//         slidesPerView: 'auto',
-//         freeMode: true,
-//     });
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
     const tickerRows = document.querySelectorAll('.ticker-row');
 
