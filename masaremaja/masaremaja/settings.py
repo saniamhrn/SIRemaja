5f644e6c94a13bd1e26729e04ffe7fb8aaeb21ab
saniarizqi <sania.rizqi@ui.ac.id>
2024-12-05 02:06:15 +0700

    chore: update STATIC_ROOT setting

--- masaremaja/masaremaja/settings.py
+++ masaremaja/masaremaja/settings.py
@@ -167,8 +167,15 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-# Media files (user uploaded files)
+# Media file management
+if DEBUG:
+    # For development, use local file storage for media files
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+else:
+    # For production, use S3 storage for media files
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+# Media files (user uploaded files)
 if DEBUG:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -183,13 +190,11 @@ else:
 if DEBUG:
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
     STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 else:
     # For production
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-    STATIC_ROOT = None
     STATICFILES_DIRS = []
+    STATIC_ROOT = None
