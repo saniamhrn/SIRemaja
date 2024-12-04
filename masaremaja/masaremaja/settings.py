10c7bb176a191c88c4c1add2623317adfe92d5b7
saniarizqi <sania.rizqi@ui.ac.id>
2024-12-05 02:10:52 +0700

    chore: update STATIC_ROOT setting

--- masaremaja/masaremaja/settings.py
+++ masaremaja/masaremaja/settings.py
@@ -197,4 +197,5 @@ else:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     STATICFILES_DIRS = []
-    STATIC_ROOT = None
+    # STATIC_ROOT = None
+    STATIC_ROOT = '/tmp/staticfiles'
