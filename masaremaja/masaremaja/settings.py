b9a680d5daab0e020e93522ca3b0d2e8d91b587a
saniarizqi <sania.rizqi@ui.ac.id>
2024-12-05 00:35:18 +0700

    chore: update setting for storage

--- masaremaja/masaremaja/settings.py
+++ masaremaja/masaremaja/settings.py
@@ -167,17 +167,6 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-STORAGES = {
-    # Media file management
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    },
-
-    "staticfiles": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    },
-}
-
 # Media files (user uploaded files)
 
 if DEBUG:
